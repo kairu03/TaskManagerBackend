15db8e70caa12694158680f21529600788c8912e
kairufrancisco <kairufrancisco@gmail.com>
2026-02-27 17:53:15 +0800

    remove unwanted comments

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,38 +1,19 @@
 // Global Error Handling Middleware
 // This middleware catches all errors passed using next(err)
-// It must be placed LAST in app.js after all routes
-
-// export const errorHandler = (err, req, res, next) => {
-
-//   // Determine the HTTP status code:
-//   // 1. Use custom error statusCode if available (from ApiError)
-//   // 2. Otherwise use the current response statusCode
-//   // 3. Default to 500 (Internal Server Error)
-//   const statusCode = err.statusCode || res.statusCode || 500;
-
-//   // Set the HTTP response status
-//   res.status(statusCode).json({ 
-
-//     // Optional: helps frontend easily detect failed requests 
-//     success: false,
-
-//     // Send error message (either custom or fallback message)
-//     message: err.message || 'Server Error',
-
-//     // Show stack trace ONLY in development for debugging
-//     // Hide it in production for security reasons
-//     stack: process.env.NODE_ENV === 'development' ? err.stack : null,
-//   });
-// };
-
+// It must be placed last in app.js after all routes
 
 export const errorHandler = (err, req, res, next) => {
-
+  // Determine the HTTP status code:
   const statusCode = err.statusCode || res.statusCode || 500;
-
-  res.status(statusCode).json({
+  // Set the HTTP response status
+  res.status(statusCode).json({ 
+    //  helps frontend easily detect failed requests 
     success: false,
-    message: err.message || 'server error',
+    // Send error message (either custom or fallback message)
+    message: err.message || 'Server Error',
+    // Show stack trace ONLY in development for debugging
+    // Hide it in production for security reasons
     stack: process.env.NODE_ENV === 'development' ? err.stack : null,
-  })
-}
+  });
+};
+
--- src/server.js
+++ src/server.js
@@ -1,7 +1,6 @@
 import { connectDB } from "./config/db.js";
 import app from "./app.js";
 
-
 const PORT = process.env.PORT || 5000;
 
 // DB connection
@@ -10,23 +9,3 @@ connectDB().then(() => {
     console.log('Server started on PORT: ', PORT);
   });
 });
-
-
-
-// const startServer = async () => {
-//   try {
-//     await connectDB();
-//     app.listen(PORT, () => {
-//       console.log(`Server running on port ${PORT}`);
-//     });
-//   } catch (error) {
-//     console.error('Failed to start server:', error);
-//     process.exit(1);
-//   }
-// };
-
-// startServer();
-
-
-
-//  "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY5ODQ4MDBiZmUyMmRkODc5Y2I5NjY4YSIsImlhdCI6MTc3MDM3ODA3MywiZXhwIjoxNzcwOTgyODczfQ.SEnQEI7FMx93K0LFCH1briEN2jsWhE5W_Z3pjsgb0Bc"
